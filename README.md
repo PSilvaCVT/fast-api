300993ff52a0f1e7b45663c50221ed5b270465c0
Pedro Silva <ps@cvt.services>
2023-02-22 19:23:12 +0000

    - django api using redis (for websockets POC).

---
+++ README.md
@@ -0,0 +1,8 @@
+# README #
+
+### Setup ###
+* on a terminal, go to this project's folder.
+* Make a python venv for this project.
+* Install requirements.txt
+* copy `.env.local` to `.env`
+* run `python manage.py runserver 8081`
---
+++ fast_api/management/commands/send_message_command.py
@@ -0,0 +1,21 @@
+import json
+
+from django.core.management.base import BaseCommand
+from django_redis import get_redis_connection
+
+
+class Command(BaseCommand):
+    help = "Send message to websocket through Redis"
+
+    def handle(self, *args, **options):
+        redis_conn = get_redis_connection("default")
+        redis_conn.publish(
+            "message_event",
+            json.dumps(
+                dict(
+                    chat_id="123",
+                    data="Test send message to websocket through Redis.",
+                )
+            ),
+
+        )
